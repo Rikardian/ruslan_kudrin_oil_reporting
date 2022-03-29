2b65213e5b41acddb9d1646c92ecd88293901cb3
Rikardian <92721408+Rikardian@users.noreply.github.com>
2022-03-29 12:19:16 +0400

    init project

---
+++ ru/ibs/ruslan_kudrin_oil_reporting/RuslanKudrinOilReportingApplication.java
@@ -0,0 +1,13 @@
+package ru.ibs.ruslan_kudrin_oil_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuslanKudrinOilReportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuslanKudrinOilReportingApplication.class, args);
+    }
+
+}
