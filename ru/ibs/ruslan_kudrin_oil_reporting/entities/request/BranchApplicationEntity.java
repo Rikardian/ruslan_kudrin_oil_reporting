f7fdd74e0de1a1026ab036c1f8d11a1f7d33a35b
Rikardian <92721408+Rikardian@users.noreply.github.com>
2022-03-30 18:46:32 +0400

    development/ rework requests, added migration script

--- ru/ibs/ruslan_kudrin_oil_reporting/entities/request/BranchApplicationEntity.java
+++ ru/ibs/ruslan_kudrin_oil_reporting/entities/request/BranchApplicationEntity.java
@@ -3,10 +3,7 @@ package ru.ibs.ruslan_kudrin_oil_reporting.entities.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 /*
 Статус заявки на уровне филлиала
@@ -19,9 +16,15 @@ public class BranchApplicationEntity {
     /*
     Id заявки
      */
-    @OneToOne
+    @Column(name = "branch_application_id")
     @Id
-    private BranchApplicationEntity applicationId;
+    private long branchApplicationId;
+
+    /*
+    Название филлиала
+     */
+    @Column(name = "branch_name")
+    private String branchName;
 
     /*
     Форма заявки
@@ -34,5 +37,11 @@ public class BranchApplicationEntity {
      */
     @Column(name = "application_status")
     private String applicationStatus;
+
+    /*
+    Дата доставки (если назначена)
+     */
+    @Column(name = "date_of_delivery")
+    private String dateOfDelivery;
 
 }
--- ru/ibs/ruslan_kudrin_oil_reporting/repository/request/HeadApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.ibs.ruslan_kudrin_oil_reporting.repository.request;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.ibs.ruslan_kudrin_oil_reporting.entities.request.HeadApplicationEntity;
-
-public interface HeadApplicationRepository extends JpaRepository<HeadApplicationEntity, Long> {
-}
